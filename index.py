bc82e8b2848c4f39246b3566d9c3de9aa2fdb743
TineVal <valentinemunyambu641@gmail.com>
2024-11-12 17:17:43 +0300

    Create index.py

---
+++ index.py
@@ -0,0 +1,16 @@
+x = 1
+y = 2.8
+z = 1j
+
+print (x)
+print (y)
+print (x+y)
+print(x-y)
+print(y-x)
+print(x*y)
+print(x/y)
+print(x+z)
+
+username = ('Ruth')
+favColour = ('Purple')
+print(username, favColour, 'is cool!')
